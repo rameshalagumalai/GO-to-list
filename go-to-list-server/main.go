e3cf974402cb6c4f7f1a344c87ac98c2a6ef8f41
rameshalagumalai <rameshalagumalai@github.com>
2024-11-15 22:17:34 +0530

    [SERVER] handle implementations based on route

--- go-to-list-server/main.go
+++ go-to-list-server/main.go
@@ -7,6 +7,19 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", handleHomeRoute)
+	http.HandleFunc("/tasks", handleTasksRoute)
+
 	fmt.Println("Server started at port 2323")
 	log.Fatal(http.ListenAndServe(":2323", nil))
+}
+
+// "/" route
+func handleHomeRoute(resp http.ResponseWriter, req *http.Request) {
+	resp.Write([]byte("Home route"))
+}
+
+// "/tasks" route
+func handleTasksRoute(resp http.ResponseWriter, req *http.Request) {
+	resp.Write([]byte("Tasks route"))
 }
