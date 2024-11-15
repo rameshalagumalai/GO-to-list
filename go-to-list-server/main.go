93b1cd2f22dcc1dbbe4a3fff718fd4fe570133bb
rameshalagumalai <rameshalagumalai@github.com>
2024-11-16 00:29:43 +0530

    handle task creation

--- go-to-list-server/main.go
+++ go-to-list-server/main.go
@@ -8,9 +8,21 @@ import (
 )
 
 type ApiResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code int `json:"code"`
+	Message string `json:"message,omitempty"`
+	Data any `json:"data"`
 }
+
+type Task struct {
+	Id int `json:"id"`
+	Name string `json:"task_name"`
+}
+
+type TaskPayload struct {
+	Name string `json:"task_name"`
+}
+
+var tasks = []Task{}
 
 func main() {
 	http.HandleFunc("/", handleHomeRoute)
@@ -33,10 +45,22 @@ func handleTasksRoute(resp http.ResponseWriter, req *http.Request) {
 	var method = req.Method
 	switch method {
 	case http.MethodGet:
-		response = ApiResponse{Code: 0, Message: "Tasks route"}
+		response = ApiResponse{Code: 0, Data: tasks}
+	case http.MethodPost:
+		var createdTask = createTask(req)
+		response = ApiResponse{Code: 0, Message: "Task added successfully", Data: createdTask}
 	default:
-		response = ApiResponse{Code: -1, Message: "The requested method is not configured for this resource"}
+		response = ApiResponse{Code: -1, Data: "The requested method is not configured for this resource"}
 	}
 
 	json.NewEncoder(resp).Encode(response)
+}
+
+func createTask(req *http.Request) Task {
+	var taskPayload TaskPayload
+	json.NewDecoder(req.Body).Decode(&taskPayload)
+
+	var createdTask = Task{Id: len(tasks)+1, Name: taskPayload.Name}
+	tasks = append(tasks, createdTask)
+	return createdTask
 }
